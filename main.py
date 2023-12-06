f958bca9e74bbc988021a243207407f551850c76
Balachandar <105204404+bala-chandar12@users.noreply.github.com>
2023-12-06 09:30:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return jsonify({"Choo Choo": "Welcome to your Flask "})
+post_example', methods=['POST'])
+def post_example():
+    if request.method == 'POST':
+        # Access the data sent in the POST request
+        data = request.get_json()  # Assuming the data is sent as JSON
+        # You can also use request.form to get form data
+        # data = request.form
+
+        # Process the data
+        # For example, if the JSON contains a key 'message'
+        if 'question' in data:
+            received_message = data['question']
+            #o=predict(received_message)
+
+            #return f"Received message: {o}"
+            return jsonify({"Received message": ok})
+        else:
+            return "No 'message' key found in the POST request data"
+    else:
+        return "This endpoint only accepts POST requests"
 
 
 if __name__ == '__main__':
